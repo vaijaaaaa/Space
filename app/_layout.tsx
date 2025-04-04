57391d36dbcc15f695c34a9534214fddd8806a0a
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-04 22:30:40 +0530

    setting up layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen name="index" options={{title: "Feed"}} />
+          <Stack.Screen name="notifications" options={{title: "Notifications"}} />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,9 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/notifications" asChild>
+        <Text style={{ color: "blue", marginTop: 20 }}>Visit Notifications Tab</Text>
+      </Link>
     </View>
   );
 }
