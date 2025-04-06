04d9f50cae07f31e31fd40c0f488d7657d68908c
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-06 12:25:01 +0530

    convex setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { ClerkLoaded, ClerkProvider } from '@clerk/clerk-expo'
 import { Slot } from 'expo-router'
 import { tokenCache } from "@/cache";
+import InitialLayout from "@/components/InitialLayout";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -16,7 +17,7 @@ export default function RootLayout() {
       <ClerkLoaded>
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1,backgroundColor:"black" }}>
-        <Stack screenOptions={{ headerShown: false }} />
+        <InitialLayout/>
       </SafeAreaView>
     </SafeAreaProvider>
     </ClerkLoaded>
---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+
+
+export default {
+    providers: [
+      {
+        domain: "https://quiet-maggot-13.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
