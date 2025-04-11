c727732e807878d180641c096f8ae9c42c4a3ac0
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-11 22:31:00 +0530

    some design in create page

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -4,7 +4,7 @@ import { useUser } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, KeyboardAvoidingView, Platform, ActivityIndicator } from 'react-native';
 
 import * as ImagePicker from 'expo-image-picker';
 
@@ -31,28 +31,68 @@ export default function CreateScreen() {
 
   }
 
-
-
-
-
-
-
-  return (
-    <View style={styles.container}>
-    <View style={styles.header}>
-      <TouchableOpacity onPress={() => router.back()}>
-        <Ionicons name="arrow-back" size={28} color={COLORS.primary} />
+  if(!selectedImage){
+    return (
+      <View style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.back()}>
+          <Ionicons name="arrow-back" size={28} color={COLORS.primary} />
+        </TouchableOpacity>
+  
+        <Text style={styles.headerTitle}>New Post</Text>
+  
+        <View style={{ width: 28 }} />
+      </View>
+  
+      <TouchableOpacity style={styles.emptyImageContainer} onPress={pickImage}>
+        <Ionicons name="image-outline" size={48} color={COLORS.grey} />
+        <Text style={styles.emptyImageText}>Tap to select an image</Text>
       </TouchableOpacity>
-
-      <Text style={styles.headerTitle}>New Post</Text>
-
-      <View style={{ width: 28 }} />
     </View>
+    );
+  }
 
-    <TouchableOpacity style={styles.emptyImageContainer} onPress={pickImage}>
-      <Ionicons name="image-outline" size={48} color={COLORS.grey} />
-      <Text style={styles.emptyImageText}>Tap to select an image</Text>
-    </TouchableOpacity>
-  </View>
+  return(
+    <KeyboardAvoidingView
+    behavior={Platform.OS === 'ios' ? 'padding' : "height"}
+    style={styles.container}
+    keyboardVerticalOffset={Platform.OS === 'ios' ? 20 : 0}
+    >
+      <View style={styles.contentContainer}>
+        <View style = {styles.header}>
+        <TouchableOpacity
+        onPress={()=>{
+          setSelectedImage(null);
+          setCaption("");
+        }}
+        disabled={isSharing}
+        >
+          <Ionicons
+          name = "close-outline"
+          size={28}
+          color={isSharing?COLORS.grey:COLORS.white}
+          />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>New Post</Text>
+        <TouchableOpacity
+  style={[styles.shareButton, isSharing && styles.shareButtonDisabled]}
+  disabled={isSharing || !selectedImage}
+>
+  {isSharing ? (
+    <ActivityIndicator size="small" color={COLORS.primary} />
+  ) : (
+    <Text style={styles.shareText}>Share</Text>
+  )}
+</TouchableOpacity>
+
+
+      </View>
+      
+      </View>
+    </KeyboardAvoidingView>
   );
+
+
+
+  
 }
