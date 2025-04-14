9ac17f4d64eb232879286468491e4e0d9ca267d3
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-14 17:11:52 +0530

    some debuggs in root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,9 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import InitialLayout from "@/components/InitialLayout";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { SplashScreen } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -9,7 +12,21 @@ if(!publishableKey) {
   throw new Error("Missing EXPO_PUBLIC_CLERK_KEY in .env");
 }
 
+
+SplashScreen.preventAutoHideAsync();
+
+
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono": require("../assets/fonts/JetBrainsMono.ttf"),
+  })
+
+  const onLayoutReadyView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+
   return (
     <ClerkAndConvexProvider>
        <SafeAreaProvider>
@@ -21,4 +38,6 @@ export default function RootLayout() {
    
    
   );
+
 }
+
