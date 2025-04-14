950344ebe675e0a69e28e619d59602b71d1297be
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-14 11:56:56 +0530

    post query added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,71 @@
-import { styles } from "@/styles/auth.styles";
+import Story from "@/components/Story";
+import { STORIES } from "@/constants/mock-data";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useQueries, useQuery } from "convex/react";
 import { Link } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {signOut} = useAuth();
+
+  const posts = useQuery(api.posts.getFeedPosts)
+
+  if(posts == undefined) return <Loader />
+
+  if(posts.length == 0) return <NoPostsFound />
+
+
   return (
     <View style={styles.container}>
+
+      <View style={styles.header}>
+      <Text style={styles.headerTitle}>spotlight</Text>
+      <TouchableOpacity onPress={() => signOut()}>
+        <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+      </TouchableOpacity>
       
+    </View>
+
+    <ScrollView showsHorizontalScrollIndicator={false}>
+      <ScrollView 
+      horizontal
+      showsHorizontalScrollIndicator={false}
+        style={styles.storiesContainer}
+        >
+
+          {STORIES.map((story)=>(
+            <Story key={story.id} story={story} />
+          ))}
+      </ScrollView>
+      {posts.map((post)=>(
+        <Post key={post._id} post={post} />
+      ))}
+
+    </ScrollView>
+
+
+
+
+    </View>
+  );
+}
+
+const NoPostsFound =() =>{
+  return (
+    <View style={{
+      flex:1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text style={{
+        fontSize: 20,
+        color: COLORS.primary,
+      }}>No posts yet</Text>
     </View>
   );
 }
