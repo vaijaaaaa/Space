2b5fc097133b7fbb7a79a17e5a281a6cba885e1e
vaijaaaaa <vaijuwalker111@gmail.com>
2025-04-15 16:59:59 +0530

    notification styles

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,7 +1,14 @@
 // app/(tabs)/notifications.tsx
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
 import { View, Text } from 'react-native';
 
 export default function Notifications() {
+
+  const notifications = useQuery(api.notifications.getNotifications);
+
+
+
   return (
     <View>
       <Text>Notifications Screen</Text>
